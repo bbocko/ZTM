31289a56896d6c8ee3c434b4e61ca86f9de8d228
blaz <blaz.bocko1@gmail.com>
2023-01-08 22:12:51 +0100

    Update js-exercise
    Implement checking prompted data from user with one in database

---
+++ js-exercise/script.js
@@ -0,0 +1,32 @@
+var database = [
+    {
+        username: "Blaz",
+        password: "supersecret"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired from all that learning."
+    },
+    {
+        username: "Sally",
+        timeline: "Javascript is soooo cool!"
+    }
+];
+
+var userNamePrompt = prompt("What is your username?");
+
+var passwordPrompt = prompt("What is your password?");
+
+function signIn(user, pass) {
+    if (user === database[0].username && 
+    pass === database[0].password) {
+        console.log(newsFeed);
+    } else {
+        alert("Sorry, wrong username or password!");
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt);
